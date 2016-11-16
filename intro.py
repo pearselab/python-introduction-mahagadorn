9f35be5e0741652138df573812f9c3172378d81f
mahagadorn <mahagadorn@aggiemail.usu.edu>
2016-11-16 10:35:53 -0700

    Intro Progress

---
+++ intro.py
@@ -0,0 +1,18 @@
+## MAHagadorn
+## Intro to python assignment
+## November 16, 2016
+
+
+## 1. Write a loop that prints out the numbers from 20 to 10
+#range will print from 0 to the maximum numbers
+# - means to make it go backwards
+# first tried for i in range(20, 10, -1); doesn't work only prints to 11.
+#Need to make the range from 20 to 9
+
+for i in range(20, 9, -1):
+    print(i)
+
+
+
+## 2. Write a list comprehension that returns the numbers from 20 to 10
+#List comprehension 
